849e24288d91fde4e873074171f4954b88cf37cd
Jose <jozylopez27@gmail.com>
2020-01-25 12:46:38 -0600

    some cool jquery stuff

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 /* eslint-disable strict */
 
 
-function handleProjectsButton () {
+function handleProjectsButton() {
   $('body').on('click', '.scrollMe', function(event) {
     $('body').scrollTo('#projScroll', 800);
   });
 }
 
 
-
-function handleConButton () {
+function handleConButton() {
   $('body').on('click', '.scrollMe2', function(event) {
     $('body').scrollTo('#conScroll', 800);
   });
 }
 
 
+function delayCollage() {
+$('.one').delay(1000).animate({'opacity': '1'}, 2000);
+$('.two').delay(2000).animate({'opacity': '1'}, 2000);
+$('.three').delay(3000).animate({'opacity': '1'}, 2000);
+$('.four').delay(4000).animate({'opacity': '1'}, 2000);
+}
+
+
 function main () {
 handleProjectsButton();
 handleConButton();
+delayCollage();
 }
 
 $(main);
