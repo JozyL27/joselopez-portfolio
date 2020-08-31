4b1f09f6ce36342ca6b0a104b5ed5fd27a6e3367
Jose <jozylopez27@gmail.com>
2020-08-31 09:37:18 -0500

    added cardigan / prettier settings

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 /* eslint-disable strict */
 
-
 function handleProjectsButton() {
-  $('body').on('click', '.scrollMe', function(event) {
-    $('body').scrollTo('#projScroll', 800);
+  $("body").on("click", ".scrollMe", function (event) {
+    $("body").scrollTo("#projScroll", 800);
   });
 }
-
 
 function handleConButton() {
-  $('body').on('click', '.scrollMe2', function(event) {
-    $('body').scrollTo('#conScroll', 800);
+  $("body").on("click", ".scrollMe2", function (event) {
+    $("body").scrollTo("#conScroll", 800);
   });
 }
 
-
 function delayCollage() {
-$('.one').delay(1000).animate({'opacity': '1'}, 2000);
-$('.two').delay(2000).animate({'opacity': '1'}, 2000);
-$('.three').delay(3000).animate({'opacity': '1'}, 2000);
-$('.four').delay(4000).animate({'opacity': '1'}, 2000);
+  $(".one").delay(1000).animate({ opacity: "1" }, 2000);
+  $(".two").delay(2000).animate({ opacity: "1" }, 2000);
+  $(".three").delay(3000).animate({ opacity: "1" }, 2000);
+  $(".four").delay(4000).animate({ opacity: "1" }, 2000);
 }
 
-
-function main () {
-handleProjectsButton();
-handleConButton();
-delayCollage();
+function main() {
+  handleProjectsButton();
+  handleConButton();
+  delayCollage();
 }
 
 $(main);
